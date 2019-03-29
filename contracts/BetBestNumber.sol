3fa73f02f16cd5b61add5931673929dc381f9296
Carlos Oporto <carlosoporto@gmail.com>
2019-03-29 18:33:34 +0100

    distribute prizes function

--- contracts/BetBestNumber.sol
+++ contracts/BetBestNumber.sol
@@ -52,4 +52,28 @@ contract BetBestNumber {
         uint256 numberGenerated = block.number % 10 + 1; // Change this, isn't secure
         distributePrizes(numberGenerated);
     }
+
+    // Sends the corresponding ether to each winner depending on the total bets
+    function distributePrizes(uint256 numberWinner) public {
+        address[100] memory winners; // We have to create a temporary in memory array with fixed size
+        uint256 count = 0; // This is the count for the array of winners
+
+      for(uint256 i = 0; i < players.lengthl i++) {
+            address playerAddress = players[i];
+            if(playerInfo[playerAddress]).numberSelected == numberWinner) {
+                winners[count] = playerAddress;
+                count++;
+            }
+            delete playerInfo[playerAddress]; // Delete all the players
+      }
+
+      players.length = 0; // Delete all the players array
+
+      uint256 winnerEtherAmount = totalBet / winners.length; // How much each winner gets
+
+      for(uint256 j = 0; j < count; j++) {
+          if(winners[j] != address(0)) // Check that the address in this fixed array is not empty
+          winners[j].transfer(winnerEtherAmount);
+      }
+    }
 }
