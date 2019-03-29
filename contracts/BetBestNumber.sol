ab4a69e13266fdc1b7594eb34cb4bccd536c3704
Carlos Oporto <carlosoporto@gmail.com>
2019-03-29 17:00:59 +0100

    Keep a record of how much a user has bet for what number.

---
+++ contracts/BetBestNumber.sol
@@ -0,0 +1,25 @@
+pragma solidity 0.4.20;
+
+contract BetBestNumber {
+    address public owner;
+    uint256 public minimumBet;
+    uint256 public totalBet;
+    uint256 public numberofBets;
+    uint256 public maxAmountfBets = 100;
+    address[] public players;
+
+    struct Player {
+      uint256 amountBet;
+      uint256 numberSelected;
+    }
+
+    // The address of the player and => the user info mapping (address=> Player) public playerInfo;
+
+    function BetBestNumber() public {
+      owner = msg.sender;
+    }
+
+    function kill() public {
+      if(msg.sender == owner) selfdestruct(owner);
+    }
+}
