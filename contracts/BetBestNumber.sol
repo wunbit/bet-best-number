315da2c18d03cf8a51aba70a0c8a0a97af4a8a14
Carlos Oporto <carlosoporto@gmail.com>
2019-03-29 17:49:09 +0100

    bet for a number between 1 and 10 both inclusive

--- contracts/BetBestNumber.sol
+++ contracts/BetBestNumber.sol
@@ -9,19 +9,33 @@ contract BetBestNumber {
     address[] public players;
 
     struct Player {
-      uint256 amountBet;
-      uint256 numberSelected;
+        uint256 amountBet;
+        uint256 numberSelected;
     }
 
     // The address of the player and => the user info
     mapping (address=> Player) public playerInfo;
 
-    function BetBestNumber() public {
-      owner = msg.sender;
-      if(_minimumBet != 0) minimumBet = _minimumBet;
+    function BetBestNumber(uint256 _minimumBet) public {
+        owner = msg.sender;
+        if(_minimumBet != 0) minimumBet = _minimumBet;
     }
 
     function kill() public {
-      if(msg.sender == owner) selfdestruct(owner);
+        if(msg.sender == owner) selfdestruct(owner);
+    }
+
+    // To bet for a number between 1 and 10 both inclusive
+    function bet(uint256 numberSelected) public payable {
+        require(!checkPlayerExists(msg.sender));
+        require(numberSelected >= 1 && numberSelected <= 10);
+        require(msg.value >= minimumBet);
+
+        playerInfo[msg.sender].amountBet = msg.value;
+        playerInfo[msg.sender].numberSelected = numberSelected;
+        numberofBets++;
+        players.push(msg.sender);
+        totalBet += msg.value;
+
     }
 }
