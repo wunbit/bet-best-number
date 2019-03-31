d7843a2a1890e367d180203d4e7dbbb8681811d7
Carlos Oporto <carlosoporto@gmail.com>
2019-03-31 18:10:59 +0200

    build for webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require('path')
 
 module.exports = {
+  mode: 'development',
   entry: path.join(__dirname, 'src/js', 'index.js'), // Our frontend wil be inside the src folder
   output: {
     path: path.join(__dirname, 'dist'),
